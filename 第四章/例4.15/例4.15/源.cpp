6defa3c9189b710e91b821d8dcd9b5e24103f302
zzhuazi <huahuajj01@aliyun.com>
2016-10-15 21:46:01 +0800

    指针与new

---
+++ 第四章/例4.15/例4.15/源.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void aa(int a)
+{
+	int* p = new int[a];
+}
+
+int main()
+{
+	int u = 6;
+	int * p;
+	p = &u;
+
+	cout << "Values: u=" << u << " *p:" << *p << endl;
+	(*p)++;
+	cout << "Values: u=" << u << " *p:" << *p << endl;
+
+	int* pa = new int;
+	*pa = 1200;
+	cout << *pa << "  " << pa << "  " << &pa << "  " << *(&pa) << endl;
+	delete pa;
+	pa = &u;
+	cout << *pa << "  " << pa;
+	aa(6);
+}
